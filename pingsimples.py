42766a09e95b56fb3365b5cbaf84d04f821f533c
MrArthurEmanuel <91098280+MrArthurEmanuel@users.noreply.github.com>
2021-09-20 21:05:15 -0300

    Add files via upload

--- pingsimples.py
+++ pingsimples.py
@@ -1,8 +1,12 @@
 import os #importa o modulo ou biblioteca OS (Integra os programas e recursos do SO)
+import time
 
-print("#" *60) #Imprimindo 60 vezes o "#"
+print('#' *31)
+print('### Testador de IP''s e Hosts ###')
+print('#' *31)
+print('\nBy. Mr.Arthur (EU MESMO)')
 
-ip_ou_host = input('Digite o IP ou HOST a ser verificado: ')
+ip_ou_host = input('\nDigite o IP ou HOST a ser verificado: ')
 #Criando uma variável para receber do usuário o IP ou HOST
 
 print("-" *60)
@@ -13,3 +17,6 @@ os.system('ping -n 6 {}'. format(ip_ou_host))
 
 print("-" *60)
 #Imprimindo o "-" 60 vezes
+
+time.sleep(2)
+os.system('pause')
